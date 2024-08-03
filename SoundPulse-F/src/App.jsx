183dc0a3db662cfccba48c4fb71ea8bfd16ef997
Vijay Krishan <vk856549@gmail.com>
2024-08-03 23:25:18 +0530

    initial commit

---
+++ SoundPulse-F/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return <div className="h-screen bg-black">  
+    <div className="h-[90%] flex">
+        <Sidebar />
+    </div>
+  </div>;
+}
+
+export default App;
---
+++ SoundPulse-F/src/Components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex '>
+        <div className='vg-[#121212] h-[15%] flex flex flex-col justify-around'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
