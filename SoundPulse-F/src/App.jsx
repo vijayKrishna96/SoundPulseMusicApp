7f79cda2b03bfac6229e7e42d560606d1a200435
Vijay Krishan <vk856549@gmail.com>
2024-08-06 22:18:47 +0530

    Modified

--- SoundPulse-F/src/App.jsx
+++ SoundPulse-F/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar";
+import Player from "./Components/Player";
+import Display from "./Components/Routes";
 
 function App() {
   return <div className="h-screen bg-black">  
     <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
     </div>
+    <Player />
   </div>;
 }
 
